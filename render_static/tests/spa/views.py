f17d4ac174c2b242f31a2dbeec9811bd17c398ea
Brian Kohan <bckohan@gmail.com>
2023-01-22 14:43:38 -0500

    temporarily remove selenium tests from CI

--- render_static/tests/spa/views.py
+++ render_static/tests/spa/views.py
@@ -6,7 +6,7 @@ class SPAIndex(TemplateView):
     template_name = 'spa/index.html'
 
     def get_context_data(self, **kwargs):
-        return {
+        return {  # pragma: no cover
             **super().get_context_data(),
             'namespace': self.request.resolver_match.namespace
         }
