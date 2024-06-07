36f30e32979f5c4e225d712523828d9ca0909776
Brian Kohan <bckohan@gmail.com>
2024-06-06 21:55:26 -0700

    fix web tests

--- tests/test_web.py
+++ tests/test_web.py
@@ -14,43 +14,93 @@ import pytest
 from django.core.management import call_command
 from django.test import LiveServerTestCase, override_settings
 from django.urls import reverse
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from contextlib import contextmanager
 
 from tests.test_core import LOCAL_STATIC_DIR, BaseTestCase
 
 logger = logging.getLogger(__name__)
 
 
-if not shutil.which("chromedriver"):  # pragma: no cover
-    pytest.skip(
-        "Web tests require chromedriver to be installed.", allow_module_level=True
-    )
-else:
-    logger.info("Using chromedriver: %s", shutil.which("chromedriver"))
-    chrome_options = Options()
-    options = [
-        "--headless",
-        "--disable-gpu",
-        "--window-size=1920,1200",
-        "--ignore-certificate-errors",
-        "--disable-extensions",
-        "--no-sandbox",
-        "--disable-dev-shm-usage",
-        "--start-maximized",
-        "--proxy-server='direct://'",
-        "--proxy-bypass-list=*",
+@contextmanager
+def web_driver(width=1920, height=1200):
+    import platform
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+    # Set up headless browser options
+    def opts(options=ChromeOptions()):
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument(f"--window-size={width}x{height}")
+        return options
+
+    def chrome():
+        from selenium.webdriver.chrome.service import Service
+        from webdriver_manager.chrome import ChromeDriverManager
+
+        return webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()), options=opts()
+        )
+
+    def chromium():
+        from selenium.webdriver.chrome.service import Service as ChromiumService
+        from webdriver_manager.chrome import ChromeDriverManager
+        from webdriver_manager.core.os_manager import ChromeType
+
+        return webdriver.Chrome(
+            service=ChromiumService(
+                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+            ),
+            options=opts(),
+        )
+
+    def firefox():
+        from selenium import webdriver
+        from selenium.webdriver.firefox.options import Options
+        from selenium.webdriver.firefox.service import Service as FirefoxService
+        from webdriver_manager.firefox import GeckoDriverManager
+
+        return webdriver.Firefox(
+            service=FirefoxService(GeckoDriverManager().install()),
+            options=opts(Options()),
+        )
+
+    def edge():
+        from selenium.webdriver.edge.options import Options
+        from selenium.webdriver.edge.service import Service as EdgeService
+        from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+        options = Options()
+        options.use_chromium = True
+        return webdriver.Edge(
+            service=EdgeService(EdgeChromiumDriverManager().install()),
+            options=opts(options),
+        )
+
+    services = [
+        chrome,
+        edge if platform.system().lower() == "windows" else chromium,
+        firefox,
     ]
-    for option in options:
-        chrome_options.add_argument(option)
-    driver = webdriver.Chrome(
-        service=Service(shutil.which("chromedriver")), options=chrome_options
-    )
+
+    driver = None
+    for service in services:
+        try:
+            driver = service()
+            break  # use the first one that works!
+        except Exception as err:
+            pass
+
+    if driver:
+        yield driver
+        driver.quit()
+    else:
+        raise RuntimeError("Unable to initialize any webdriver.")
 
 
-@pytest.mark.skip(reason="Disabled")
 @override_settings(
     INSTALLED_APPS=[
         "tests.spa",
@@ -83,20 +133,21 @@ class TestMultipleURLTreeSPAExample(BaseTestCase, LiveServerTestCase):
         call_command("collectstatic")
 
     def test_example_pattern(self):
-        driver.get(f'{self.live_server_url}{reverse("spa1:index")}')
-        from pprint import pprint
+        with web_driver() as driver:
+            driver.get(f'{self.live_server_url}{reverse("spa1:index")}')
+            from pprint import pprint
 
-        pprint(driver.get_log("browser"))
-        elem = driver.find_element(By.ID, "qry-result")
-        self.assertEqual(json.loads(elem.text)["request"], "/spa1/qry/")
-        elem = driver.find_element(By.ID, "qry-result-arg")
-        self.assertEqual(json.loads(elem.text)["request"], "/spa1/qry/5")
+            pprint(driver.get_log("browser"))
+            elem = driver.find_element(By.ID, "qry-result")
+            self.assertEqual(json.loads(elem.text)["request"], "/spa1/qry/")
+            elem = driver.find_element(By.ID, "qry-result-arg")
+            self.assertEqual(json.loads(elem.text)["request"], "/spa1/qry/5")
 
-        driver.get(f'{self.live_server_url}{reverse("spa2:index")}')
-        elem = driver.find_element(By.ID, "qry-result")
-        self.assertEqual(json.loads(elem.text)["request"], "/spa2/qry/")
-        elem = driver.find_element(By.ID, "qry-result-arg")
-        self.assertEqual(json.loads(elem.text)["request"], "/spa2/qry/5")
+            driver.get(f'{self.live_server_url}{reverse("spa2:index")}')
+            elem = driver.find_element(By.ID, "qry-result")
+            self.assertEqual(json.loads(elem.text)["request"], "/spa2/qry/")
+            elem = driver.find_element(By.ID, "qry-result-arg")
+            self.assertEqual(json.loads(elem.text)["request"], "/spa2/qry/5")
 
     # def tearDown(self):
     #     pass
